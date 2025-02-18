936ee4d90e4994d602e030583221dd47cc67c3c1
gabrielgonta <146724165+gabrielgonta@users.noreply.github.com>
2025-02-18 21:53:12 +0100

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/PWeatherApp/' // Remplace par ton nom de repo GitHub
+    : '/',
     baseUrl: '/progressive-weather-app/',
     pwa: {
         themeColor: '#6CB9C8',
